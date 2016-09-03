d30c876e5f1893cb0692da72b7b716ff569a8e3e
Dave G <davegutz@alum.mit.edu>
2016-09-03 10:38:12 -0400

    Find min update = 200 us

--- myESC-Particle-DEV/potESC.ino
+++ myESC-Particle-DEV/potESC.ino
@@ -57,27 +57,29 @@ SYSTEM_THREAD(ENABLED);       // Make sure heat system code always run regardles
 // #define CONSTANT
 #define PWM_PIN          A4                 // PWM output (A4)
 #define POT_PIN          A0                 // Potentiometer input pin on Photon (A0)
-#define CONTROL_DELAY    1UL               // Control law wait, ms
-#define PUBLISH_DELAY    40UL               // Time between cloud updates (10000), ms
-#define READ_DELAY       1UL               // Sensor read wait (5000, 100 for stress test), ms
-#define QUERY_DELAY      15000UL            // Web query wait (15000, 100 for stress test), ms
+#define CONTROL_DELAY    200UL              // Control law wait (), micros
+#define PUBLISH_DELAY    40000UL            // Time between cloud updates (), micros
+#define READ_DELAY       200UL              // Sensor read wait (1000, 100 for stress test), micros
+#define QUERY_DELAY      1500000UL          // Web query wait (15000000, 100 for stress test), micros
 #ifndef BARE_PHOTON
-  #define FILTER_DELAY   1UL             // In range of tau/4 - tau/3  * 1000, ms
+  #define FILTER_DELAY   200UL              // In range of tau/4 - tau/3  * 1000, ms
 #else
-  #define FILTER_DELAY   1UL             // In range of tau/4 - tau/3  * 1000, ms
+  #define FILTER_DELAY   200UL              // In range of tau/4 - tau/3  * 1000, ms
 #endif
 //
 // Dependent includes.   Easier to debug code if remove unused include files
 #include "myFilters.h"
 
 // Global variables
-LagTustin*          throttleFilter;         // Exponential rate lag filter
+LagTustin*          throttleFilter1;         // Exponential rate lag filter
+LagTustin*          throttleFilter2;         // Exponential rate lag filter
 Servo               myservo;  // create servo object to control a servo
 int                 numTimeouts     = 0;    // Number of Particle.connect() needed to unfreeze
 int                 potValue        = 2872; // Dial raw value, 0-4096
-const  double       tau             = 0.5;  // Filter time constant, sec
-int                 throttle        = 0;    // Pot value, 0-179 degrees
-int                 throttle_filt   = 0;    // Pot value, 0-179 degrees
+const  double       tau             = 0.2;  // Filter time constant, sec
+double              throttle        = 0;    // Pot value, 0-179 degrees
+double              throttle1       = 0;    // Pot value, 0-179 degrees
+double              throttle_filt   = 0;    // Pot value, 0-179 degrees
 double              updateTime      = 0.0;  // Control law update time, sec
 int                 verbose         = 3;    // Debugging Serial.print as much as you can tolerate.  0=none
 
@@ -87,9 +89,10 @@ void setup()
   Serial.begin(9600);
   myservo.attach(PWM_PIN);  // attaches the servo.  Only supported on pins that have PWM
   pinMode(POT_PIN, INPUT);
-  // Rate filter
-  throttleFilter  = new LagTustin(float(CONTROL_DELAY)/1000.0, tau, -0.1, 0.1);
-  delay(5000);
+  // Lag filter
+  throttleFilter1  = new LagTustin(float(CONTROL_DELAY)/1000000.0, tau, -0.1, 0.1);
+  throttleFilter2  = new LagTustin(float(CONTROL_DELAY)/1000000.0, tau, -0.1, 0.1);
+  delay(1000);
   if (verbose>1) Serial.printf("\nCalibrating ESC...");
   while (throttle<179)
   {
@@ -108,12 +111,12 @@ void setup()
   Serial.printf("To flash code to this device, push and hold both Photon buttons, release RESET until purple observed, then release.\n");
 
   WiFi.off();
-  delay(5000);
+  delay(1000);
 }
 
 void loop() {
   unsigned long           currentTime;        // Time result
-  unsigned long           now = millis();     // Keep track of time
+  unsigned long           now = micros();     // Keep track of time
   bool                    control;            // Control sequence, T/F
   bool                    filter;             // Filter for temperature, T/F
   bool                    publish;            // Publish, T/F
@@ -143,7 +146,7 @@ void loop() {
   filter    = ((now-lastFilter)>=FILTER_DELAY) || RESET>0;
   if ( filter )
   {
-    tFilter     = float(now-lastFilter)/1000.0;
+    tFilter     = float(now-lastFilter)/1000000.0;
     if ( verbose > 3 ) Serial.printf("Filter update=%7.3f\n", tFilter);
     lastFilter    = now;
   }
@@ -152,7 +155,7 @@ void loop() {
 
   // Interrogate pot; run fast for good tactile feedback
   // my pot puts out 0- 4096 observed using Tinker
-  if ( checkPot )
+  if ( checkPot && RESET<1 )
   {
 #ifndef BARE_PHOTON
     potValue    = analogRead(POT_PIN);
@@ -163,8 +166,9 @@ void loop() {
   if ( filter )
   {
     if ( verbose>4 ) Serial.printf("FILTER\n");
-    throttle_filt = (int)throttleFilter->calculate(double(throttle), RESET, tFilter);
-    if ( verbose > 4) Serial.printf("throttle=%f, RESET=%d, tFilter=%f a=%f b=%f rate=%f state=%f filt=%f\n", throttle, RESET, tFilter, throttleFilter->a(), throttleFilter->b(), throttleFilter->rate(), throttleFilter->state(), throttle_filt);
+    throttle1     = (int)throttleFilter1->calculate(throttle,  RESET, tFilter);
+    throttle_filt = (int)throttleFilter2->calculate(throttle1, RESET, tFilter);
+    if ( verbose > 4) Serial.printf("throttle=%f, RESET=%d, tFilter=%f a=%f b=%f rate=%f state=%f filt=%f\n", throttle, RESET, tFilter, throttleFilter2->a(), throttleFilter2->b(), throttleFilter2->rate(), throttleFilter2->state(), throttle_filt);
     RESET = 0;
   }
 
@@ -173,14 +177,14 @@ void loop() {
   control = (deltaT >= CONTROL_DELAY);
   if ( control  )
   {
-    updateTime    = float(deltaT)/1000.0 + float(numTimeouts)/100.0;
+    updateTime    = float(deltaT)/1000000.0 + float(numTimeouts)/100000.0;
     lastControl   = now;
-    myservo.write(throttle);                // sets the servo position according to the scaled value
+    myservo.write(throttle_filt);                // sets the servo position according to the scaled value
   }
 
 
   if ( publish )
   {
-    if (verbose>1) Serial.printf("Throttle Filt=%ld, updateTime=%f\n", throttle_filt, updateTime);
+    if (verbose>1) Serial.printf("Throttle Filt=%4.2f, updateTime=%7.5f\n", throttle_filt, updateTime);
   }
 }
